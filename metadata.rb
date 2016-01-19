8139c93fb43914e697bdb27b629cd645ebb6cef3
Joshua Halickman <jhalickman@gmail.com>
2016-01-19 15:37:51 -0500

    Adding tls as protocol

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
-name             "papertrail"
+name             "chef-papertrail"
 maintainer       "First Banco"
 maintainer_email "rob@firstbanco.com"
 license          "All rights reserved"
 description      "Installs/Configures papertrail"
 long_description IO.read(File.expand_path('../README.md', __FILE__))
-version          "0.0.3"
+version          "0.0.4"
 depends          "build-essential"
 depends          "rsyslog"
 depends			 "remote_syslog2"
